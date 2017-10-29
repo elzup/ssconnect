9770576ccd4080ce5d8f3581fde13d151c0ac1f7
elzup <guild0105@gmail.com>
2017-10-30 04:08:38 +0900

    Solve Dependency

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 // @flow
 
-import camelcaseKeys from 'camelcase-keys'
+import camelcaseKeysRecursive from 'camelcase-keys-recursive'
 import { normalizeStories } from './normalize'
 import request from 'superagent'
 import _ from 'lodash'
@@ -78,7 +78,7 @@ export async function getStories(
 
   // { stories: res.data, pageInfo: FeedClient.getPageInfo(res) }
   const normalizedData = normalizeStories(res.body)
-  const camelizedData = camelcaseKeys(normalizedData, { deep: true })
+  const camelizedData = camelcaseKeysRecursive(normalizedData, { deep: true })
   const pageInfo = getPageInfo(res)
 
   return {
@@ -107,5 +107,5 @@ export async function getTags(timeout: number = TIMEOUT): Promise<Tag[]> {
     })
   })
 
-  return _.values(camelcaseKeys(res.body, { deep: true }))
+  return _.values(camelcaseKeysRecursive(res.body, { deep: true }))
 }
