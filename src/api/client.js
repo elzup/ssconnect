0679d184a8ab9ac6de1761697b8732f6beb59b52
elzup <guild0105@gmail.com>
2017-10-29 16:56:45 +0900

    CORS response headers

--- src/api/client.js
+++ src/api/client.js
@@ -1,9 +1,9 @@
 // @flow
 
-import { create } from 'apisauce'
 import camelcaseKeys from 'camelcase-keys'
 import _ from 'lodash'
 import { normalizeStories } from './normalize'
+import request from 'superagent'
 
 import type {
   Story,
@@ -14,6 +14,13 @@ import type {
   Article,
 } from '../types'
 
+const UseHeader = {
+  page: 'X-Page',
+  total: 'X-Total-Pages',
+  next: 'X-Next-Page',
+  prev: 'X-Prev-Page',
+}
+
 const host =
   process.env.NODE_ENV == 'development'
     ? // ? 'http://localhost:3001'
@@ -21,10 +28,11 @@ const host =
     : 'https://ssconnect.elzup.com'
 const TIMEOUT = 1000
 
-const api = create({
-  baseURL: host,
-  timeout: TIMEOUT,
-})
+const requestHeaders = _.values(UseHeader).join(', ')
+
+const baseHeaders = {
+  'Content-Type': 'application/json',
+}
 
 function permitQuery(screen: Screen): QueryParams {
   switch (screen.type) {
@@ -55,19 +63,33 @@ export async function getStories(
   timeout: number = TIMEOUT,
 ): Promise<GetStoriesCallback> {
   const params = permitQuery(screen)
-  const res = await api.get('/v1/stories', params)
+  const storiesRequest = request
+    .get(host + '/v1/stories')
+    .query(params)
+    .set(baseHeaders)
+  const res = await new Promise((resolve, reject) => {
+    storiesRequest.end((err, res) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(res)
+      }
+    })
+  })
+
   // { stories: res.data, pageInfo: FeedClient.getPageInfo(res) }
-  const normalizedData = normalizeStories(res.data)
+  const normalizedData = normalizeStories(res.body)
   const camelizedData = camelcaseKeys(normalizedData, { deep: true })
   const pageInfo = getPageInfo(res)
+
   return { ..._.mapValues(camelizedData.entities, _.values), pageInfo }
 }
 
 function getPageInfo(res: any): PageInfo {
   return {
-    page: parseInt(res.headers['x-page'], 10),
-    total: parseInt(res.headers['x-total-pages'], 10),
-    next: parseInt(res.headers['x-next-page'], 10) || false,
-    prev: parseInt(res.headers['x-prev-page'], 10) || false,
+    page: parseInt(res.header[UseHeader.page], 10),
+    total: parseInt(res.header[UseHeader.total], 10),
+    next: parseInt(res.header[UseHeader.next], 10) || false,
+    prev: parseInt(res.header[UseHeader.prev], 10) || false,
   }
 }
